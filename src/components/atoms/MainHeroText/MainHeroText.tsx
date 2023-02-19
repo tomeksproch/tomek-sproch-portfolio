91f132f53689e59adbc292bcd34c160478b8f324
tomeksproch <tomek.sproch@gmail.com>
2023-02-19 12:32:09 +0100

    little changes of text

--- src/components/atoms/MainHeroText/MainHeroText.tsx
+++ src/components/atoms/MainHeroText/MainHeroText.tsx
@@ -5,8 +5,8 @@ const MainHeroText = () => {
   return (
     <MainText>
       <p>
-        And I'm a frontend developer. This website showcases my skills, works,
-        and resume. I'm excited to share my journey with you. Let's go!
+        I'm a frontend developer. This website showcases my skills, works, and
+        resume. I'm excited to share my journey with you. Let's go!
       </p>
     </MainText>
   );
--- src/components/atoms/SkillsText/SkillsText.tsx
+++ src/components/atoms/SkillsText/SkillsText.tsx
@@ -5,7 +5,7 @@ const SkillsText = () => {
     <div>
       I have a foundation in HTML, CSS, JavaScript, React, Styled Components,
       Redux, and Git, and I also have experience with Angular from my internship
-      at SOLDI Finance. As a team player with strong English language skills (B2
+      at SOLDI Finance. As a team player with English language skills (B2
       level), I am excited to collaborate with others and contribute to exciting
       projects. You can see my skills in action through the projects featured on
       this website.
