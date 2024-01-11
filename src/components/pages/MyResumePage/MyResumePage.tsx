b4dd903c7238a09116998de188aeb756f5c3ee54
tomeksproch <tomek.sproch@gmail.com>
2024-01-11 13:38:34 +0100

    cv

--- src/components/pages/MyResumePage/MyResumePage.tsx
+++ src/components/pages/MyResumePage/MyResumePage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Button from "../../atoms/Button/Button";
 import { MyResumePageWrapper, FiledContentCv } from "./MyResumePage.styles";
+
 
 const MyResumePage = () => {
   return (
@@ -8,7 +8,6 @@ const MyResumePage = () => {
       <FiledContentCv>
         Sorry, this content will be filled soon! 😢
       </FiledContentCv>
-      <Button tittle="Download my CV!" />
     </MyResumePageWrapper>
   );
 };
